96f258da2f6aea75ac6300f8fc22c008b163cf76
Agasi Miroyan <limekick@mail.ru>
2026-05-13 13:02:59 +0300

    readme added

--- app/config.py
+++ app/config.py
@@ -43,7 +43,7 @@ class Settings(BaseSettings):
         url = f"{self.DB_DRIVER}://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.POSTGRES_DB}"
         return url
 
-    model_config = SettingsConfigDict(env_file=[".env.dev"])
+    model_config = SettingsConfigDict(env_file=[".env.prod"])
 
 
 settings = Settings()
